0ac222055d9d8becab1f2686dc1f9b8923c67e62
Shaddowalker-26 <chosentuguchi@gmail.com>
2026-05-24 21:25:41 +0000

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,36 +2,26 @@
 
 set -e
 
-
 rm -rf .repo/local_manifests
 
-
 repo init -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs
-
 
 git clone https://github.com/Shaddowalker-26/android_local_manifests.git \
 -b main \
 .repo/local_manifests
 
-
-if [ -f /usr/bin/resync ]; then
-  /usr/bin/resync
-else
-  /opt/crave/resync.sh
-fi
-
+repo sync -c --no-tags --no-clone-bundle -j$(nproc --all)
 
 export BUILD_USERNAME=Shaddowalker-26
 export BUILD_HOSTNAME=crave
 
-
-source build/envsetup.sh
-
+git clone https://${KEYS_PAT}@github.com/Shaddowalker-26/android_vendor_keys.git \
+vendor/lineage-priv/keys
 
 export ANDROID_SIGNING_KEYS=vendor/lineage-priv/keys
 
+source build/envsetup.sh
 
 lunch lineage_spes-trunk_staging-userdebug
-
 
 mka bacon
